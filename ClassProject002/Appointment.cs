65b4a2602f6f827afbff5824b6fdab30b81614df
vincent <vincent.herff@marquette.edu>
2024-04-30 20:44:38 -0500

    added roomNumber to appointment class, added MakeAppointment method to program class but still needs to be worked on

--- ClassProject002/Appointment.cs
+++ ClassProject002/Appointment.cs
@@ -5,6 +5,7 @@ public class Appointment
     private static int autoIncrement;
     public int Id {get;}
     public DateTime dateTime {get; set;}
+    public int roomNumber {get; set;}
 
     public Appointment()
     {
